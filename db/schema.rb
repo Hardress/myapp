2eab913352bcf8071c0ea9b91ac353b505ab0677
Madeline <mjsmith01@email.wm.edu>
2018-10-24 07:53:34 -0400

    4.2 final edits

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_19_182401) do
+ActiveRecord::Schema.define(version: 2018_10_24_114535) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2018_10_19_182401) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "color"
+    t.integer "price"
   end
 
 end
