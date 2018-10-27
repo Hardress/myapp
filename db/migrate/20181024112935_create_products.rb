6891b36b10bd40042e190db78004b9a317f7312d
Madeline <mjsmith01@email.wm.edu>
2018-10-27 00:04:17 -0400

    4.2 migration changes

--- db/migrate/20181024112935_create_products.rb
+++ db/migrate/20181024112935_create_products.rb
@@ -4,8 +4,6 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.string :name
       t.text :description
       t.string :image_url
-      t.string :color
-      t.integer :price
       t.timestamps
     end
   end
