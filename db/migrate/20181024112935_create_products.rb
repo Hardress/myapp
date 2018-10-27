63e26ac6c482d1af09f7c614f053318f8a35299d
Madeline <mjsmith01@email.wm.edu>
2018-10-27 00:13:06 -0400

    4.2 migration unless

--- db/migrate/20181024112935_create_products.rb
+++ db/migrate/20181024112935_create_products.rb
@@ -1,10 +1,12 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
-    create_table :products do |t|
-      t.string :name
-      t.text :description
-      t.string :image_url
-      t.timestamps
+    unless table_exists? :products
+      create_table :products do |t|
+        t.string :name
+        t.text :description
+        t.string :image_url
+        t.timestamps
+      end
     end
   end
 end
