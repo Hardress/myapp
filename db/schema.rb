5f6b078a94d7e03a447fe682f546dff08d55151d
Madeline <mjsmith01@email.wm.edu>
2018-11-01 17:04:59 -0400

    4.6 changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_184217) do
+ActiveRecord::Schema.define(version: 2018_11_01_134214) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
