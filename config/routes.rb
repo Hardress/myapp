32e306f5818bc583e6e49536b6d4ee684c9fffe0
Madeline <mjsmith01@email.wm.edu>
2018-11-19 13:40:35 -0500

    5.8 changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
 
   devise_for :users
   # landing page reroute
-  resources :users
-  resources :products do
+resources :products do
   resources :comments
 end
+
+resources :users
+resources :products
+resources :orders, only: [:index, :show, :create, :destroy]
 get 'simple_pages/about'
 get 'simple_pages/contact'
 get 'simple_pages/index'
@@ -14,6 +17,5 @@ get 'simple_pages/landing_page'
 get 'simple_pages/thank_you'
   post 'simple_pages/thank_you'
 root 'simple_pages#index'
-
-resources :orders, only: [:index, :show]
+mount ActionCable.server => '/cable' 
 end
