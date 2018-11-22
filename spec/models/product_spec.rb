f83288b02f9fee4561f6903cabaafad147e4d5bb
Madeline <mjsmith01@email.wm.edu>
2018-11-22 15:28:26 -0500

    6.2 changes

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe Product do
+   let(:product) {Product.create!(name:"Wine", description:"I am a description", image_url:"about.overview_1.jpg" )}
+
+   before do
+     product.comments.create!(rating:1, user:@user, body:"Awful wine!")
+     product.comments.create!(rating:3, user:@user, body:"Ok wine!")
+     product.comments.create!(rating:5, user:@user, body:"Great wine!")
+end
+
+  it "returns the highest rating comment" do
+    expect(product.highest_rating_comment.rating).to eq 5
+    expect(product.highest_rating_comment.body).to eq "Great wine!"
+  end
+
+  it "returns the lowest rating comment" do
+    expect(product.lowest_rating_comment.rating).to eq 1
+    expect(product.lowest_rating_comment.body).to eq "Awful wine!"
+  end
+
+  it "returns the average rating of all comments" do
+    expect(product.average_rating).to eq 3
+  end
+
+  it "is not valid without a comment" do
+    expect(Product.new(description: "I am a description")).not_to be_valid
+  end
+end
